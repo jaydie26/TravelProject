db70cd87b6508daaf7384fcd5b3f7c0e696fcbb8
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-10-21 15:49:39 +0700

    new update 3:49:PM 21/10/2020

--- Content/js/booking.js
+++ Content/js/booking.js
@@ -1,16 +1,25 @@
-const tabItems=document.querySelectorAll(".tab-item");
-const tabContentItems=document.querySelectorAll(".tab-content-item");
-function selectItem(e){
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+function selectItem(e) {
     removeBorder();
     removeShow();
     this.classList.add("tab-border")
-    const tabContentItem=document.querySelector(`#${this.id}-content`);
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
     tabContentItem.classList.add('show');
 }
-function removeShow(){
-    tabContentItems.forEach(item=>item.classList.remove("show"));
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove("show"));
 }
-function removeBorder(){
-    tabItems.forEach(item=>item.classList.remove("tab-border"));
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove("tab-border"));
 }
-tabItems.forEach(item => item.addEventListener("click",selectItem));
+tabItems.forEach(item => item.addEventListener("click", selectItem));
+
+function insertCustomer() {
+    var x = document.getElementById("tablecustomers").rows.length;
+    document.getElementById("tablecustomers").insertRow(-1).innerHTML = '<tr><td>' + x + '</td><td><input type="text"class="fullname"></td><td><input type="date" class="dateofb"></td><td><input type="text" class="adr"></td><td><select name="sex" class="sex"><option value="#">Male</option><option value="#">Female</option></select></td><td><select name="typecustomer" class="typecustomer"><option value="#">VietNamese</option><option value="#">Overseas Vietnamese</option><option value="#">Foreigner</option></select></td><td><input type="text" class="age"></td><td><input type="text" class="pri"></td></tr><td><button class="trash fas fa-trash"onclick="deletecustomer(this)"></button></td>';
+}
+function deletecustomer(x) {
+    var i = x.parentNode.parentNode.rowIndex;
+    document.getElementById("tablecustomers").deleteRow(i);
+}
